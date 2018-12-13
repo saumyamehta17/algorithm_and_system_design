f7a472129393c2e7bca9c483d1b3ca8a7a0d2c83
saumya <smehta1289@gmail.com>
2018-12-13 15:46:39 +0530

    refinement balanced tree algo

--- test_algo.rb
+++ test_algo.rb
@@ -1,126 +1,58 @@
-# Avl Tree is self-balacing tree where diff b\w height of left and 
-# right subtree is not greater than 1
-# If tree is skewed, then operations(search, min, max, delete, insert) will go to o(n)
-# Avl tree make it o(logn)
+Node = Struct.new(:data, :left, :right)
+class Tree
+  attr_accessor :root, :balanced
 
-Node = Struct.new(:value, :height, :left, :right) do 
-         def initialize(*)
-          super
-          self.height = 1
-         end  
-      end
+  def initialize
+    @balanced = true
+  end  
 
-class AvlTree
-  attr_accessor :root
-
-  def height(node)
-    if node.nil? 
+  def isBalanced(node)
+    if !balanced
       return 0
-    end
-    node.height 
-  end 
-
-  def get_balance(node)
-    height(node.left) - height(node.right)
-  end 
-
-  def rightRotation(node)
-    x = node.left
-    tmp = x.right
-
-    x.right = node
-    node.left = tmp
-    x
-  end 
-
-  def leftRotation(node)
-    x = node.right
-    tmp = x.left
-
-    x.left = node
-    node.right = tmp
-    x
-  end 
-
-  def insert(node, value)
+    end  
 
     if node.nil?
-      return Node.new(value)
-    elsif value < node.value
-      node.left = insert(node.left, value)
-    elsif value > node.value
-      node.right = insert(node.right, value)
-    end 
+      return 0
+    end
 
-    node.height = 1 + [height(node.left), height(node.right)].max
+    left_h = isBalanced(node.left)
+    right_h = isBalanced(node.right)
 
-    balance = get_balance(node)
+    if (left_h - right_h).abs > 1
+      @balanced = false
+      return 0
+    end
 
-    # if balance > 1 || balance < -1
-    #     puts "#{node.value} --> Not a Avl"
-    # end 
+    [left_h, right_h].max + 1  
+  end
 
-    if balance > 1 && value < node.left.value
-      puts "Doing Balancing using Left Left Rotation required"
-      return rightRotation(node)
-    end 
+  def isBalancedIterative(node)
+    nodes = []
+    depths = []
+    nodes.push([node, 0])
+    while(!nodes.empty?)
+      node, depth = nodes.pop
+      if node.left.nil? && node.right.nil?
+        depths << depth if !depths.include?(depth)
+        if depths.length > 2 || (depths[1].to_i - depths[0].to_i).abs > 1
+          return false
+        end
+      else
+        nodes.push([node.left, depth+1]) if !node.left.nil?
+        nodes.push([node.right, depth+1]) if !node.right.nil?
+      end    
+    end
+    return true  
+  end  
+end
 
-    if balance < -1 && value > node.right.value
-      puts "Doing Balancing using Right Right Rotation required at #{node.value}"
-      return leftRotation(node)
-    end 
-
-    if balance > 1 && value > node.left.value
-      puts "Doing Balancing using Left Right Rotation required"
-      node.left = leftRotation(node.left)
-      return rightRotation(node)
-    end 
-
-    if balance < -1 && value < node.right.value
-      puts "Doing Balancing using Right Left Rotation required at #{node.value}"
-      node.right = rightRotation(node.right)
-      return leftRotation(node)
-    end 
-
-    node
-  end 
-
-  def print_tree(node)
-    print_tree(node.left) if !node.left.nil?
-    puts "value #{node.value} and height #{node.height}"
-    print_tree(node.right) if !node.right.nil?
-  end 
-end 
-
-tree = AvlTree.new   
-
-# Left Left Rotation example
-# tree.root = tree.insert(tree.root, 10)
-# tree.root = tree.insert(tree.root, 5)
-# tree.root = tree.insert(tree.root, 1)
-
-# Right Right Rotation example
-# tree.root = tree.insert(tree.root, 10)
-# tree.root = tree.insert(tree.root, 5)
-# tree.root = tree.insert(tree.root, 15)
-# tree.root = tree.insert(tree.root, 20)
-# tree.root = tree.insert(tree.root, 25)
-
-# Right Left Rotation example
-# tree.root = tree.insert(tree.root, 10)
-# tree.root = tree.insert(tree.root, 5)
-# tree.root = tree.insert(tree.root, 15)
-# tree.root = tree.insert(tree.root, 20)
-# tree.root = tree.insert(tree.root, 19)
-
-# Left Right Rotation example
-tree.root = tree.insert(tree.root, 2)
-tree.root = tree.insert(tree.root, 1)
-tree.root = tree.insert(tree.root, 5)
-tree.root = tree.insert(tree.root, 7)
-tree.root = tree.insert(tree.root, 9)
-tree.root = tree.insert(tree.root, 3)
-tree.root = tree.insert(tree.root, 6)
-tree.root = tree.insert(tree.root, 8)
-puts tree.root
-# tree.print_tree(tree.root)
+tree = Tree.new
+tree.root = Node.new(1)
+tree.root.left = Node.new(2)
+tree.root.right = Node.new(3)
+tree.root.left.left = Node.new(4)
+tree.root.left.left.left = Node.new(5)
+# puts "Tree is Balanced #{tree.balanced}"
+# tree.isBalanced(tree.root)
+# puts "Tree is Balanced #{tree.balanced}"
+puts tree.isBalancedIterative(tree.root)
--- tree/is_balanced_tree.rb
+++ tree/is_balanced_tree.rb
@@ -1,104 +1,60 @@
-class Node
-  attr_accessor :value, :left, :right
+Node = Struct.new(:data, :left, :right)
 
-  def initialize(val)
-    @value = val
-  end  
-end  
+class Tree
+  attr_accessor :root, :balanced
 
-class BinaryTree
-  attr_accessor :root, :is_bal
-  def initialize(val)
-    @root = Node.new(val)
-    @is_bal = true
+  def initialize
+    @balanced = true
   end  
 
-  def self.balanced_sample
-    bt = BinaryTree.new(1)
-    _tree = bt.root 
-    _tree.left = Node.new(2)
-    _tree.right = Node.new(3)
-    _tree.left.left = Node.new(4)
-    _tree.left.right = Node.new(5)
-    bt
-  end
+  def isBalanced(node)
+    if !balanced
+      return 0
+    end  
 
-  def self.unbalanced_sample
-    bt = BinaryTree.new(1)
-    _tree = bt.root
-    _tree.left = Node.new(2)
-    _tree.right = Node.new(3)
-    _tree.left.left = Node.new(4)
-    _tree.left.left.left = Node.new(8)
-    _tree.left.right = Node.new(5)
-    bt
-  end  
-
-  def self.unbalanced_sample2
-    bt = BinaryTree.new(1)
-    _tree = bt.root
-    _tree.right = Node.new(3)
-    _tree.right.right = Node.new(4)
-    _tree.right.right = Node.new(5)
-    bt
-  end  
-
-  def isBalancedRecursion?(node)
-    if(node.nil?)
+    if node.nil?
       return 0
     end
 
-    l = isBalanced?(node.left)
-    r = isBalanced?(node.right)  
+    left_h = isBalanced(node.left)
+    right_h = isBalanced(node.right)
 
-    if (l-r).abs > 1
-      return false
-    else
-      return [l,r].max+1
+    if (left_h - right_h).abs > 1
+      @balanced = false
+      return 0
     end
+
+    [left_h, right_h].max + 1  
   end  
 
-  def self.is_balanced?(node)
-
+  def isBalancedIterative(node)
     nodes = []
     depths = []
-
-    nodes.push([node, 0]) #node and height
-
+    nodes.push([node, 0])
     while(!nodes.empty?)
-      if(node.nil?)
-        return true
-      end  
       node, depth = nodes.pop
-
-      if(node.left.nil? && node.right.nil?)
-        depths.push(depth) unless depths.include?(depth)
-        
-        # if depths length is 1 its still handle as nil.to_i
-        if(depths.length > 2 || (depths[0].to_i - depths[1].to_i).abs > 1)
+      if node.left.nil? && node.right.nil?
+        depths << depth if !depths.include?(depth)
+        if depths.length > 2 || (depths[1].to_i - depths[0].to_i).abs > 1
           return false
-        end  
+        end
       else
-        if(node.left)
-          nodes.push([node.left, depth+1])
-        end  
-        if(node.right)
-          nodes.push([node.right, depth+1])
-        end  
-      end  
-
+        nodes.push([node.left, depth+1]) if !node.left.nil?
+        nodes.push([node.right, depth+1]) if !node.right.nil?
+      end    
     end
-
-    return true
-  end  
+    return true  
+  end
 end  
 
 # tree = BinaryTree.balanced_sample
-tree = BinaryTree.unbalanced_sample2
+tree = Tree.new
+tree.root = Node.new(1)
+tree.root.left = Node.new(2)
+tree.root.right = Node.new(3)
+tree.root.left.left = Node.new(4)
+tree.root.left.left.left = Node.new(5)
 
-# tree.is_balanced_with_recursion(tree.root)
-# puts tree.is_bal
-# tree = BinaryTree.unbalanced_sample
-# tree = BinaryTree.balanced_sample
-res = tree.isBalancedRecursion?(tree.root)
-res ? (puts "Yeah Balanced") : (puts "Uhhhh Not balanced")
+tree.isBalanced(tree.root)
+puts "Recursion: Tree is Balanced = #{tree.balanced}"
+puts "Iterative: Tree is Balanced = #{tree.isBalancedIterative(tree.root)}"
