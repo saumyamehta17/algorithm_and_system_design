fb856aeb79c82452321df68d0c539e60994d4d2a
saumya <smehta1289@gmail.com>
2019-06-16 08:03:34 +0700

    BS: edge cases handled

--- searching/binary_search.rb
+++ searching/binary_search.rb
@@ -1,17 +1,29 @@
 def binary_search(arr, low, hi, val)
-  while(low <= hi)
-    mid = (low + hi)/2
-    if(val == arr[mid])
-      puts "#{val} Found at #{mid}"
-      return
-    elsif (val > arr[mid])
-      low = mid + 1
-    elsif (val < arr[mid])
-      hi = mid - 1
+  if low > hi
+    raise 'Limits reversed'
+  end
+  
+  loop do
+    if arr[low] > arr[hi]
+      raise 'Array not sorted'
     end
+
+    center = (low + hi) / 2
+    if (low == hi && arr[center] != val)
+      raise 'Element not found'
+    end
+
+    if arr[center] == val
+      puts "#{val} Found at #{center}"
+      return
+    elsif val > arr[center]
+      low = center + 1
+    else
+      hi = center - 1
+    end  
   end  
-  puts "Not Found"
 end
 
-arr = [10,20,21,35,40,88,90]
+# arr = [10,20,21,35,40,88,90]
+arr = [88,12,90,100]
 binary_search(arr, 0, arr.length-1, 88)
