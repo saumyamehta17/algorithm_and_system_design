84afccc25664496d954277b2577094e9c1d6d74d
saumya <smehta1289@gmail.com>
2019-08-29 17:27:20 +0700

    stock and buy go easy now

--- array/stock_buy_n_sell.rb
+++ array/stock_buy_n_sell.rb
@@ -43,3 +43,25 @@ end
 s = StockBuySell.new([100, 180, 260, 310, 40, 535, 695])
 s.process
 s.print_it
+
+# new easy version
+def stock_days(arr)
+  n = arr.length
+  i = 0
+  while(i < n)
+    buy = i
+    sell = i
+    while(sell < n-1 && arr[sell] <= arr[sell+1])
+      sell += 1
+    end
+
+    if buy < sell
+      puts "(#{buy}, #{sell})"
+    end  
+    i = sell + 1
+  end  
+end  
+arr = [23, 13, 25, 29, 33, 19, 34, 45, 65, 67]
+arr = [100, 180, 260, 310, 40, 535, 695]
+arr = [45, 35, 25, 10, 40, 535, 695]
+stock_days(arr)
