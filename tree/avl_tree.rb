eaa312a861b26b2e09b4c09c53035e1a4349953f
Saumya Mehta <smehta1289@gmail.com>
2020-04-08 06:33:59 +0200

    start red black

--- tree/avl_tree.rb
+++ tree/avl_tree.rb
@@ -1,28 +1,28 @@
-# Avl Tree is self-balacing tree where diff b\w height of left and 
+# Avl Tree is self-balacing tree where diff b\w height of left and
 # right subtree is not greater than 1
 # If tree is skewed, then operations(search, min, max, delete, insert) will go to o(n)
 # Avl tree make it o(logn)
 
-Node = Struct.new(:value, :height, :left, :right) do 
+Node = Struct.new(:value, :height, :left, :right) do
 				 def initialize(*)
 				 	super
 				 	self.height = 1
-				 end	
+				 end
 	   	end
 
 class AvlTree
 	attr_accessor :root
 
 	def height(node)
-		if node.nil? 
+		if node.nil?
 			return 0
 		end
-		node.height	
-	end	
+		node.height
+	end
 
 	def get_balance(node)
 		height(node.left) - height(node.right)
-	end	
+	end
 
 	def rightRotation(node)
 		x = node.left
@@ -31,7 +31,7 @@ class AvlTree
 		x.right = node
 		node.left = tmp
 		x
-	end	
+	end
 
 	def leftRotation(node)
 		x = node.right
@@ -40,7 +40,7 @@ class AvlTree
 		x.left = node
 		node.right = tmp
 		x
-	end	
+	end
 
 	def insert(node, value)
 
@@ -50,7 +50,7 @@ class AvlTree
 			node.left = insert(node.left, value)
 		elsif value > node.value
 			node.right = insert(node.right, value)
-		end	
+		end
 
 		node.height = 1 + [height(node.left), height(node.right)].max
 
@@ -58,41 +58,41 @@ class AvlTree
 
 		# if balance > 1 || balance < -1
 		# 		puts "#{node.value} --> Not a Avl"
-		# end	
+		# end
 
 		if balance > 1 && value < node.left.value
 			puts "Doing Balancing using Left Left Rotation required"
 			return rightRotation(node)
-		end	
+		end
 
 		if balance < -1 && value > node.right.value
 			puts "Doing Balancing using Right Right Rotation required"
 			return leftRotation(node)
-		end	
+		end
 
 		if balance > 1 && value > node.left.value
 			puts "Doing Balancing using Left Right Rotation required"
 			node.left = leftRotation(node.left)
 			return rightRotation(node)
-		end	
+		end
 
 		if balance < -1 && value < node.right.value
 			puts "Doing Balancing using Right Left Rotation required"
 			node.right = rightRotation(node.right)
 			return leftRotation(node)
-		end	
+		end
 
 		node
-	end	
+	end
 
 	def print_tree(node)
 		print_tree(node.left) if !node.left.nil?
 		puts "value #{node.value} and height #{node.height}"
 		print_tree(node.right) if !node.right.nil?
-	end	
-end	
+	end
+end
 
-tree = AvlTree.new   
+tree = AvlTree.new
 
 # Left Left Rotation example
 # tree.root = tree.insert(tree.root, 10)
--- tree/red_black_tree.rb
+++ tree/red_black_tree.rb
@@ -1,1 +1,6 @@
-red_black_tree.rb
+# Rules
+# 1) Root is Black
+# 2) No red-red parent-child
+# 3) Number of black nodes from root to node
+
+
